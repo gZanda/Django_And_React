af3aa74091732ae9c71047deeb4067f4bbfba33e
gZanda <gabrielfzanda@gmail.com>
2023-09-18 14:50:13 -0300

    RestFramework & Cors installation

--- crud_project/crud_project/settings.py
+++ crud_project/crud_project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', # new middleware
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -124,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Who can connect to the API
+CORS_ALLOWED_ORIGINS = [
+]
