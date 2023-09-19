fc547b6d5ec0dcc2ce503044052faf7111c863e2
gZanda <gabrielfzanda@gmail.com>
2023-09-19 00:53:57 -0300

    GET ALL visible on the Front =)

--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -1,13 +1,18 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { getAllTasks } from "../api/ApiFunctions"
 
 export function TaskList() {
 
+    // This is a state variable 
+    const [tasks, setTasks] = useState([]);
+
+    // This is a side effect (runs after the render)
     useEffect(() => {
         
         async function loadTasks(){
             const res = await getAllTasks();
             console.log(res);
+            setTasks(res.data);
         }
 
         loadTasks();
@@ -15,7 +20,14 @@ export function TaskList() {
     }, [])
 
     return (
-        <h1> Task List </h1>
+        <div>
+            {tasks.map((task) => (
+                <div key={task.id}>
+                    <h2>{task.title}</h2>
+                    <p>{task.description}</p>
+                </div>
+            ))}
+        </div>
     )
 
 }
