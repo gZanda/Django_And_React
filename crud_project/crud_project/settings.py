02f8990cd136ddc78725887660b638de5dbed57e
gZanda <gabrielfzanda@gmail.com>
2023-09-22 18:32:10 -0300

    Postgree SQL integration

--- crud_project/crud_project/settings.py
+++ crud_project/crud_project/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'crud_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_tasks_app',
+        'USER': 'zaaz',
+        'PASSWORD': '5318',
+        'HOST': 'localhost', 
+        'PORT': '5432',            
     }
 }
 
